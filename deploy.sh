cfcaa2f2a49c6e67b173ee82b81ac0b4e0c0332c
Arjmandi <arjmandi.re@gmail.com>
2020-08-20 15:50:26 +0430

    updated

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 sudo docker kill hello_world_service_api_1
 sudo docker kill hello_world_service_web_1
+sudo docker kill hello_world_service_db_1
 sudo docker-compose build
 sudo docker-compose up -d
