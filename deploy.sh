cc900dde394e77ea82866e59c9b16da83a72aaa3
Arjmandi <arjmandi.re@gmail.com>
2020-08-21 23:24:56 +0430

    updated

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 docker-compose down -v
 docker-compose -f docker-compose.yml up -d --build
 # docker-compose -f docker-compose.yml exec web python manage.py migrate --noinput
+docker-compose -f docker-compose.yml exec api python manage.py collectstatic --no-input --clear
