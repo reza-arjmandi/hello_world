1cd51696feb66a5aa7eb285b9c2d094d9551573d
Arjmandi <arjmandi.re@gmail.com>
2020-08-27 17:38:37 +0430

    HomePage packground was updated

--- hello_world_frontend/src/components/HomePage.js
+++ hello_world_frontend/src/components/HomePage.js
@@ -9,6 +9,19 @@ export default function HomePage({
     return (
         <div style={{
             backgroundImage: background_image ,
+            'background-repeat':'no-repeat',
+            'position':'fixed',
+            '-webkit-background-size': 'cover',
+            '-moz-background-size': 'cover',
+            '-o-background-size': 'cover',
+            'background-size': 'cover',
+            'background-attachment': 'fixed',
+            'height': window.innerHeight,
+            top:'0px',
+            left:'0px',
+            width:'100%',
+            height:'100%',
+            'z-index':'1',
           }}>
           <HomePageCard 
             video_url={video_url} 
