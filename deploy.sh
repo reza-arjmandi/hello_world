c11f87551bd8fd272c3cd26e39b6c8832c825245
Arjmandi <arjmandi.re@gmail.com>
2020-09-18 08:46:37 +0430

    fix and issue

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 docker-compose down -v
-docker-compose -f docker-compose.pord.yml up -d --build
+docker-compose -f docker-compose.pord.yml up -d --build --force-recreate
 # docker-compose -f docker-compose.pord.yml exec web python manage.py migrate --noinput
 docker-compose -f docker-compose.pord.yml exec api python manage.py collectstatic --no-input --clear
