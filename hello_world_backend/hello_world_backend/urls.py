028552f5e8afca4dd46aacec44874f00bfdf55dd
r.arjmandi <r.arjmandi@faraz.com>
2020-09-28 18:07:51 +0330

    updated

--- hello_world_backend/hello_world_backend/urls.py
+++ hello_world_backend/hello_world_backend/urls.py
@@ -19,39 +19,39 @@ from django.contrib.auth.models import User, Group
 from django.contrib import admin
 
 
-admin.autodiscover()
+# admin.autodiscover()
 
-from rest_framework import generics, permissions, serializers
+# from rest_framework import generics, permissions, serializers
 
-from oauth2_provider.contrib.rest_framework import TokenHasReadWriteScope, TokenHasScope
+# from oauth2_provider.contrib.rest_framework import TokenHasReadWriteScope, TokenHasScope
 
-# first we define the serializers
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ('username', 'email', "first_name", "last_name")
+# # first we define the serializers
+# class UserSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = User
+#         fields = ('username', 'email', "first_name", "last_name")
 
-class GroupSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Group
-        fields = ("name", )
+# class GroupSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Group
+#         fields = ("name", )
 
-# Create the API views
-class UserList(generics.ListCreateAPIView):
-    permission_classes = [permissions.IsAuthenticated, TokenHasReadWriteScope]
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
+# # Create the API views
+# class UserList(generics.ListCreateAPIView):
+#     permission_classes = [permissions.IsAuthenticated, TokenHasReadWriteScope]
+#     queryset = User.objects.all()
+#     serializer_class = UserSerializer
 
-class UserDetails(generics.RetrieveAPIView):
-    permission_classes = [permissions.IsAuthenticated, TokenHasReadWriteScope]
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
+# class UserDetails(generics.RetrieveAPIView):
+#     permission_classes = [permissions.IsAuthenticated, TokenHasReadWriteScope]
+#     queryset = User.objects.all()
+#     serializer_class = UserSerializer
 
-class GroupList(generics.ListAPIView):
-    permission_classes = [permissions.IsAuthenticated, TokenHasScope]
-    required_scopes = ['groups']
-    queryset = Group.objects.all()
-    serializer_class = GroupSerializer
+# class GroupList(generics.ListAPIView):
+#     permission_classes = [permissions.IsAuthenticated, TokenHasScope]
+#     required_scopes = ['groups']
+#     queryset = Group.objects.all()
+#     serializer_class = GroupSerializer
 
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
@@ -77,9 +77,9 @@ class CustomAuthToken(ObtainAuthToken):
 urlpatterns = [
     path('', include('api.urls')),
     path('admin/', admin.site.urls),
-    path('o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
-    path('users/', UserList.as_view()),
-    path('users/<pk>/', UserDetails.as_view()),
-    path('groups/', GroupList.as_view()),
+    # path('o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
+    # path('users/', UserList.as_view()),
+    # path('users/<pk>/', UserDetails.as_view()),
+    # path('groups/', GroupList.as_view()),
     url(r'^api-token-auth/', CustomAuthToken.as_view()),
 ]
