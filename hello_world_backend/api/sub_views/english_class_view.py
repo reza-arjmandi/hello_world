4103c5d509775c5730b13351560794bc7c208458
arjmandi <arjmandi.re@gmail.com>
2020-12-29 16:31:44 +0330

    An issue was fixed in email content

--- hello_world_backend/api/sub_views/english_class_view.py
+++ hello_world_backend/api/sub_views/english_class_view.py
@@ -19,10 +19,13 @@ class EnglishClassViewSet(ModelViewSet):
         _class = serializer.save(owner=self.request.user)
         send_mail(
             'Your English Class',
+            "<html><body>"
             "<b>Congratulations.</b><br/>"
             "<b>You've created the English class successfully.</b><br/>"
             "<b>You can share, edit and delete your class through following link:</b><br/>"
-            f"https://www.halloenglish.com/c/classes/{_class.id}",
+            f"https://www.halloenglish.com/c/classes/{_class.id}"
+            "</body></html>"
+            ,
             'HelloWorld@halloenglish.com',
             [self.request.user],
             fail_silently=False,
